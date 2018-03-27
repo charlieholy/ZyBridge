a197b7f9f7f4fd824283eb0fe736dd1780e95b27
charlieholy <357483226@qq.com>
2018-03-27 15:34:00 +0800

    reconn

--- test/ZyConn.js
+++ test/ZyConn.js
@@ -70,39 +70,52 @@ let pako = require('pako');
 const io = require('socket.io-client');
 // or with import syntax
 //import io from 'socket.io-client';
-var socket = io.connect('http://183.131.180.105:55118',{query:{loginName:'quota_user04',password:'cjle4aly'}});
-socket.on('connect', function(msg){
-    console.log("connect: " + msg)
-    setTimeout(function(){
-        //socket.emit("subscribe-price",[commidityNamekey["美原油1805"],commidityNamekey["澳币1806"]]);
-        socket.emit("load-produt")
-        console.log("emit")
-    },3000);
-});
-socket.on("load-produt",function (data) {
-    console.log(data)
-    J_data = JSON.parse(data);
-    if(J_data.content)
-    {
-        var con_ = J_data.content;
-        var j_con = JSON.parse(con_);
-        socket.emit("subscribe-price",j_con)
+var conn = function () {
+    var socket = io.connect('http://183.131.180.105:55118',{query:{loginName:'quota_user04',password:'cjle4aly'}});
+    socket.on('connect', function(msg){
+        console.log("connect: " + msg)
+        setTimeout(function(){
+            //socket.emit("subscribe-price",[commidityNamekey["美原油1805"],commidityNamekey["澳币1806"]]);
+            socket.emit("load-produt")
+            console.log("emit")
+        },3000);
+    });
+    socket.on("load-produt",function (data) {
+        console.log(data)
+        J_data = JSON.parse(data);
+        if(J_data.content)
+        {
+            var con_ = J_data.content;
+            var j_con = JSON.parse(con_);
+            socket.emit("subscribe-price",j_con)
+        }
+    })
+    socket.on("publish-price",function(data){
+        var b64 = new Buffer(data, 'base64')
+        var json = pako.inflate(new Uint8Array(b64), {to: 'string'});
+        var sss = decodeURIComponent(json)
+        console.log(sss)
+    })
+    socket.on("subscribe-price",function (data) {
+        console.log("subscribe-price: " +  data);
+        socket.close()
+    })
+    socket.on("error",function (err) {
+        reconn()
+        console.log("error:" + err)
+    })
+    socket.on("disconnect",function () {
+        reconn()
+        console.log('disconnect')
+    })
+    var isDis = false;
+    var reconn = function(){
+        if(!isDis)
+        {   console.log("reconn");
+            isDis = true;
+            setTimeout(conn, 1000);
+        }
     }
+}
 
-})
-
-socket.on("publish-price",function(data){
-    var b64 = new Buffer(data, 'base64')
-    var json = pako.inflate(new Uint8Array(b64), {to: 'string'});
-    var sss = decodeURIComponent(json)
-    console.log(sss)
-})
-socket.on("subscribe-price",function (data) {
-    console.log("subscribe-price: " +  data);
-})
-socket.on("error",function (err) {
-    console.log("error:" + err)
-})
-socket.on("disconnect",function () {
-    console.log('disconnect')
-})
+conn();
